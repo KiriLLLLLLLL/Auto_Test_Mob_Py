dc894dc94f98d35a735f70587c8447f9d531eb5f
yanax <yanax@mail.ru>
2023-05-22 21:56:54 +0300

    add Hw1

---
+++ Lesson1/Hw1/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import requests
+import yaml
+
+with open('data.yaml') as f:
+    data = yaml.safe_load(f)
+
+name = data['user']
+passwd = data['pass']
+
+@pytest.fixture()
+def login():
+    r = requests.post('https://test-stand.gb.ru/gateway/login', data={'username': name, 'password': passwd})
+    return r.json()['token']
+
+@pytest.fixture()
+def resp():
+    return 500
+
+@pytest.fixture()
+def descr():
+    return "Такого поста еще не было"
---
+++ Lesson1/Hw1/test_03.py
@@ -0,0 +1,31 @@
+import requests
+import yaml
+from conftest import login
+
+with open('data.yaml') as f:
+    data = yaml.safe_load(f)
+
+url = data['url']
+
+
+def addPost(token):
+    params = {
+        "title": "Новый пост",
+        "description": "Такого поста еще не было",
+        "content": "Пушкабомба"
+    }
+
+    res = requests.post(url, headers={'X-Auth-Token': token}, json=params)
+    return res.status_code
+
+def checkPost(token):
+    g = requests.get('https://test-stand.gb.ru/api/posts', headers={'X-Auth-Token': token})  # , 'page': 17
+    listcont = [i['description'] for i in g.json()['data']]
+    return listcont
+
+
+def test_03(login, resp):
+    assert resp == addPost(login)
+
+def test_04(login, descr):
+    assert descr in checkPost(login)
